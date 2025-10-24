aeed32f9f772cd846346f341c06b700f103dfeaa
Borys <boris02374@gmail.com>
2025-10-25 00:49:33 +0300

    added class Teacher

---
+++ task1.py
@@ -0,0 +1,44 @@
+# Визначення класу Teacher
+class Teacher:
+    def __init__(self, first_name, last_name, age, email, can_teach_subjects):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.email = email
+        self.can_teach_subjects = can_teach_subjects
+
+    def best_teacher():
+        # subjects
+        pass
+
+    def free_subjects():
+        pass
+
+    def current_teachers_rating():
+        pass
+
+
+def create_schedule(subjects, teachers):
+    best_teacher()
+    pass
+
+
+if __name__ == "__main__":
+    # Множина предметів
+    subjects = {}
+    # Створення списку викладачів
+    teachers = []
+
+    # Виклик функції створення розкладу
+    schedule = create_schedule(subjects, teachers)
+
+    # Виведення розкладу
+    if schedule:
+        print("Розклад занять:")
+        for teacher in schedule:
+            print(
+                f"{teacher.first_name} {teacher.last_name}, {teacher.age} років, email: {teacher.email}"
+            )
+            print(f"   Викладає предмети: {', '.join(teacher.assigned_subjects)}\\n")
+    else:
+        print("Неможливо покрити всі предмети наявними викладачами.")
