c98c6fff0c79fe2814fd09e940041f82b2222a18
Borys <boris02374@gmail.com>
2025-10-26 00:10:19 +0300

    added test_list

--- task1.py
+++ task1.py
@@ -5,7 +5,9 @@ class Teacher:
         self.last_name = last_name
         self.age = age
         self.email = email
-        self.can_teach_subjects = can_teach_subjects
+        self.can_teach_subjects = set(can_teach_subjects)
+
+        self.assigned_subjects = set()
 
     def best_teacher():
         # subjects
@@ -17,17 +19,65 @@ class Teacher:
     def current_teachers_rating():
         pass
 
+    def __repr__(self):
+        return f"Teacher({self.last_name}, {self.age} років, Skills: {len(self.can_teach_subjects)})"
+
 
 def create_schedule(subjects, teachers):
-    best_teacher()
-    pass
+    for teacher in teachers:
+
+        # best_teacher()
+        pass
 
 
 if __name__ == "__main__":
     # Множина предметів
-    subjects = {}
+    subjects = {"Математика", "Фізика", "Хімія", "Інформатика", "Біологія"}
     # Створення списку викладачів
-    teachers = []
+    teachers = [
+        Teacher(
+            first_name="Олександр",
+            last_name="Іваненко",
+            age=45,
+            email="o.ivanenko@example.com",
+            can_teach_subjects={"Математика", "Фізика"},
+        ),
+        Teacher(
+            first_name="Марія",
+            last_name="Петренко",
+            age=38,
+            email="m.petrenko@example.com",
+            can_teach_subjects={"Хімія"},
+        ),
+        Teacher(
+            first_name="Сергій",
+            last_name="Коваленко",
+            age=50,
+            email="s.kovalenko@example.com",
+            can_teach_subjects={"Інформатика", "Математика"},
+        ),
+        Teacher(
+            first_name="Наталія",
+            last_name="Шевченко",
+            age=29,
+            email="n.shevchenko@example.com",
+            can_teach_subjects={"Біологія", "Хімія"},
+        ),
+        Teacher(
+            first_name="Дмитро",
+            last_name="Бондаренко",
+            age=35,
+            email="d.bondarenko@example.com",
+            can_teach_subjects={"Фізика", "Інформатика"},
+        ),
+        Teacher(
+            first_name="Олена",
+            last_name="Гриценко",
+            age=42,
+            email="o.grytsenko@example.com",
+            can_teach_subjects={"Біологія"},
+        ),
+    ]
 
     # Виклик функції створення розкладу
     schedule = create_schedule(subjects, teachers)
